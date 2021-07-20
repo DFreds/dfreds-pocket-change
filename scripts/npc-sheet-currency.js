d84fe35bf42765451dad5578f5b1301332624fff
Derek Fredrickson <dfreds08@gmail.com>
2021-07-20 01:03:17 -0400

    Support tidy sheet

--- scripts/npc-sheet-currency.js
+++ scripts/npc-sheet-currency.js
@@ -9,21 +9,37 @@ export default class NpcSheetCurrency {
    * Injects the currency row into an NPC sheet
    */
   async injectCurrencyRow() {
-    const content = $(await this._getTemplate());
+    if (this._isDefaultSheet) {
+      this._handleDefaultInjection();
+    } else if (this._isTidySheet) {
+      this._handleTidyInjection();
+    }
+  }
+
+  get _isDefaultSheet() {
+    return this._app.template.includes('npc-sheet.html');
+  }
+
+  get _isTidySheet() {
+    return this._app.template.includes('tidy5e-npc.html');
+  }
+
+  async _handleDefaultInjection() {
+    const content = $(await this._getDefaultTemplate());
 
     content
       .find('.rollable[data-action]')
       .on('click', this._onSheetAction.bind(this));
 
-    const inventoryFilters = this._html.find(
+    const injectionPoint = this._html.find(
       '.sheet-body .features .inventory-filters'
     );
-    inventoryFilters.prepend(content);
+    injectionPoint.prepend(content);
   }
 
-  async _getTemplate() {
+  async _getDefaultTemplate() {
     return renderTemplate(
-      'modules/dfreds-pocket-change/templates/npc-currency-row.html',
+      'modules/dfreds-pocket-change/templates/default-npc-currency-row.html',
       {
         data: this._data.data,
         config: {
@@ -33,6 +49,37 @@ export default class NpcSheetCurrency {
             ep: game.i18n.localize('DND5E.CurrencyEP'),
             sp: game.i18n.localize('DND5E.CurrencySP'),
             cp: game.i18n.localize('DND5E.CurrencyCP'),
+          },
+        },
+      }
+    );
+  }
+
+  async _handleTidyInjection() {
+    const content = $(await this._getTidyTemplate());
+
+    content
+      .find('.rollable[data-action]')
+      .on('click', this._onSheetAction.bind(this));
+
+    const injectionPoint = this._html.find(
+      '.sheet-body .attributes .center-pane .spellbook-title'
+    );
+    injectionPoint.before(content);
+  }
+
+  async _getTidyTemplate() {
+    return renderTemplate(
+      'modules/dfreds-pocket-change/templates/tidy-npc-currency-row.html',
+      {
+        data: this._data.data,
+        config: {
+          currencies: {
+            pp: game.i18n.localize('TIDY5E.CurrencyAbbrPP'),
+            gp: game.i18n.localize('TIDY5E.CurrencyAbbrGP'),
+            ep: game.i18n.localize('TIDY5E.CurrencyAbbrEP'),
+            sp: game.i18n.localize('TIDY5E.CurrencyAbbrSP'),
+            cp: game.i18n.localize('TIDY5E.CurrencyAbbrCP'),
           },
         },
       }
